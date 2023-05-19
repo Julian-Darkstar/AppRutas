f907ea0f8bdf889faf09237fd038670c103f6f46
Julian-Darkstar <93355425+Julian-Darkstar@users.noreply.github.com>
2023-05-19 14:56:12 -0600

    Se agrega la ruta no. 15 como pagina

---
+++ resources/views/Rutas/Ruta15LatinoSilva.blade.php
@@ -0,0 +1,10 @@
+@extends('layout')
+
+@section('content')
+<center>
+        <h1>Ruta No. 15   Latino-Silva</h1>
+    <div class="mapa">
+    <iframe src="https://www.google.com/maps/d/u/4/embed?mid=1OAwouQ4bjZlXpLpW3V2KmYsDi0CwMBA&ehbc=2E312F" width="640" height="480"></iframe>
+    </div>
+</center>
+@endsection
